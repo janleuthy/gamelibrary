ced6b8455324428850baaac29afc96782e664956
bnauer <ruben.nauer@bluewin.ch>
2020-01-22 13:36:07 +0100

    To Fix: Hamburger

--- templates/header.php
+++ templates/header.php
@@ -8,6 +8,13 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/css/style.css" >
 
+    <!-- Java Script Libraries. -->
+    <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha384-tsQFqpEReu7ZLhBV2VZlAu7zcOV+rXbYlF2cqB8txI/8aZajjp4Bqd+V6D5IgvKT" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+
     <title><?= $title; ?> | Game Library</title>
   </head>
   <body>
@@ -27,7 +34,7 @@
                         <a class="nav-link" href="/default/kontakt">Kontakt</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">Spiele</a>
+                        <a class="nav-link" href="/spiele/all">Spiele</a>
                     </li>
                 </ul>
             </div>
@@ -46,6 +53,9 @@
                 </ul>
             </div>
         </nav>
+        <header class="py-5 bg-image-full" id="sensewallpaper">
+            <img class="img-fluid d-block mx-auto" src="/images/logo.png" alt="">
+        </header>
     </header>
 
     <main>
