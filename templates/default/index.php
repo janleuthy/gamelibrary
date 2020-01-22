cb5c4aab6a73d269fdf3af697f46f27a6bbae217
janleuthy <janleuthy@gmx.ch>
2020-01-22 15:42:14 +0100

    Home, Ueber Uns, Kontakt
    To Fix: Hamburger, Space h1

--- templates/default/index.php
+++ templates/default/index.php
@@ -4,7 +4,7 @@
         <p class="lead">Ihre Leidenschaft - Auf einem Blick</p>
         <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid, suscipit, rerum quos facilis repellat architecto commodi officia atque nemo facere eum non illo voluptatem quae delectus odit vel itaque amet.
-            <a href="#">Spiele hinzufügen</a>
+            <a href="/game/create">Spiele hinzufügen</a>
         </p>
     </div>
 </section>
